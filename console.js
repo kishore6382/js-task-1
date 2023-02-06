afa09d2160ae8e5459d4d03674c3fcf71dd1ea97
kishore6382 <kishorekumar06047@gmail.com>
2023-02-06 02:16:15 -0800

    update

---
+++ console.js
@@ -0,0 +1,52 @@
+//addition
+var a=4,b=5;
+var c=a+b;
+console.log(c);
+//substraction
+var a=10,b=5;
+var c=a-b;
+console.log(c);
+//multification
+var a=3,b=4,c=2;
+var d=a*b*c;
+console.log(d);
+//divition
+ var a=12,b=4,c=3;
+ var d=a/b/c;
+ console.log(d);
+ //area of the circle
+ var pi=3.14,r=3;
+var c=pi*r**2;
+console.log(c);
+//triangle
+var b=3,h=4;
+var t=1/2*3*4;
+console.log(t);
+//simple interest
+ var p=5,n=4,r=15;
+ var si=p*n*r/100;
+ console.log(si);
+ //square
+  var a=5;
+ var sq=a**2;
+ console.log(sq);
+ //cube
+  var a=10;
+ var cu=a**3;
+ console.log(cu);
+ //total average
+   var a=6,b=5,c=4;
+ var d=a+b+c/3;
+ console.log(d);
+ //program for large number 
+ var a=6,b=5;
+ var c=a>b;
+ console.log(c);
+ //program for greater than 10 
+ var a=4;
+ var b=a>10;
+ console.log(b);
+ //divisible 5
+ var x=250;
+ var y=x%5;
+ console.log(y);
